2b8440d28320e3860304bb56f318198b97eae88d
zoushicheng <xdrc11@gmail.com>
2017-06-29 17:01:58 +0800

    [changed] remove files not use

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import {div, h1} from '@cycle/dom'
 import xs from 'xstream'
-import sampleCombine from 'xstream/extra/sampleCombine'
 import {html} from 'snabbdom-jsx'
 import isolate from '@cycle/isolate'
 import Label from './label'
