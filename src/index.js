76f79e1f467535b6df3250917ec5320ade098d07
zoushicheng <xdrc11@gmail.com>
2017-06-29 16:58:42 +0800

    [changed] counter example

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import {run} from '@cycle/run'
 import {makeDOMDriver} from '@cycle/dom'
-// import {App} from './app'
-import {App} from './drag'
+import {App} from './app'
 
 const main = App
 
 const drivers = {
   DOM: makeDOMDriver('#app'),
-  BWDOM: makeDOMDriver('#bw'),
-  LabelDOM: makeDOMDriver('#label'),
 }
 
 run(main, drivers)
